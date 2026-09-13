475f37ba35e2b2313b992ea31dd3f9081e4b3f1d
Umar Abdulla Ansari <anilansari52@gmail.com>
2026-09-14 03:01:35 +0545

    navbar green

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg sticky top-0 z-50">
+    <nav className="bg-green-700 shadow-lg sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <div className="flex-shrink-0 flex items-center ml-12">
